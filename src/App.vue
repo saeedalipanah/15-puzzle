cca99025272a3a85b5c920dfca3d81661ae6ff96
saeed alipanah <saeedalipanah1374@gmail.com>
2021-12-17 21:57:47 +0330

    win is possible

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-<div class="wrapper">
-  <fiftin-puzzle></fiftin-puzzle>
-</div>
+  <div class="wrapper">
+    <transition name="win">
+      <div v-if="victory" class="win">
+        <div class="title">Congratulations! You win🎉</div>
+        <div @click="reload" class="reload">Try again <i class="fas fa-redo"></i></div>
+      </div>
+    </transition>
+    <fiftin-puzzle @victory="win"></fiftin-puzzle>
+    <div class="construction">Use arrow keys to move blocks</div>
+  </div>
 </template>
 
 <script>
@@ -9,26 +16,93 @@ import FiftinPuzzle from './components/FiftinPuzzle.vue'
 export default {
   name: 'App',
  data(){
-
+   return{
+     victory : false,
+   }
  } ,
  components:{
    FiftinPuzzle,
+ },
+ methods:{
+   win(e){
+     this.victory = e
+   },
+   reload(){
+     window.location.reload();
+   }
  }
 }
 </script>
 
 <style lang="scss">
-*{
+* {
   margin: 0;
   padding: 0;
 }
-.wrapper{
-height: 100vh;
-display: flex;
-justify-content: center;
-align-items: center;
-position: relative;
-    perspective: 800px;
-    perspective-origin: 50%;
+.wrapper {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  perspective: 800px;
+  perspective-origin: 50%;
+  .win {
+    position: absolute;
+    top: 240px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    height: 120px;
+    color: white;
+    border-radius: 5px;
+    z-index: 5;
+    background: linear-gradient(to right, #b7094c, #61c2ad);
+    .title {
+      font-size: 280%;
+      margin-bottom: 5px;
+      white-space: nowrap;
+    }
+    .reload{
+      color: black;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 20px;
+      padding: 5px 10px;
+      border: 1px solid red;
+      border-radius: 5px;
+      transition: all .5s ease;
+      cursor: pointer;
+      i{
+        margin-left: 5px;
+      }
+      &:hover{
+        transform: scale(1.2);
+        background: linear-gradient(to right, #b7094c, #61c2ad);
+        color: white;
+        i{
+          transition: all .5s ease;
+          transform: scale(1.3) rotate(360deg);
+        }
+      }
+    }
+  }
+  .construction {
+    font-size: 30px;
+    margin-top: 40px;
+  }
+}
+.win-enter-from {
+  transform: translateY(-300px) rotate(-180deg);
+}
+.win-enter-active {
+  transition: all 1s ease-in-out;
+}
+.win-enter-to {
+  transform: translateY(0px) rotate(0deg);
 }
 </style>
--- src/components/FiftinPuzzle.vue
+++ src/components/FiftinPuzzle.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="puzzle-wrapper">
     <div class="items" v-for="(item, index) in items" :key="index">
-      <div class="item" :class="item==-1 ? 'empty-block': ''">{{ item }}</div>
+      <div class="item" :class="item == -1 ? 'empty-block' : ''">
+        {{ item }}
+      </div>
     </div>
   </div>
 </template>
@@ -10,17 +12,16 @@
 export default {
   data() {
     return {
-      items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,-1],
-      victory:true,
+      items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, -1],
+      victory: true,
     };
   },
   mounted() {
-    window.addEventListener("keydown", this.movement);
-    this.shuffle(this.items,15);
+    this.winIsPossible();
     this.win();
+    window.addEventListener("keydown", this.movement);
   },
-  created(){
-  },
+  created() {},
   methods: {
     movement(e) {
       let emptyBlock = this.items.indexOf(-1);
@@ -76,26 +77,38 @@ export default {
           this.items[emptyBlock] = this.items[emptyBlock + 1];
           this.items[emptyBlock + 1] = v;
         }
-        this.win();
+      this.win();
     },
-     shuffle(array, len = array.length) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
+    shuffle(array, len = array.length) {
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array.slice(0, len);
+    },
+    win() {
+      this.victory = true;
+      this.items.forEach((element, i) => {
+        if (element != -1 && element != i + 1) {
+          this.victory = false;
         }
-        return array.slice(0, len);
+      });
+      if (this.victory === true) {
+        this.$emit("victory", this.victory);
+      }
     },
-    win(){
-      this.victory =true;
-      this.items.forEach((element,i) => {
-       if(element != -1 && element != i+1){
-         this.victory =false;
-       }
-     });
-     if(this.victory === true){
-       alert('you win!')
-     }
-    }
+    winIsPossible() {
+      let conunter = 1;
+      while (conunter % 2 !== 0) {
+        this.shuffle(this.items, 15);
+        conunter = 0;
+        for (let i = 0; i < this.items.length; i++) {
+          if (this.items[i] > this.items[i + 1]) {
+            conunter++;
+          }
+        }
+      }
+    },
   },
 };
 </script>
@@ -111,29 +124,29 @@ export default {
   grid-gap: 0.8rem;
   background-color: #b7094c;
   border-radius: 1rem;
-  transform: rotateX(50deg);
-   box-shadow: rgba(0, 0, 0, 0.5) 0px 30px 5px, rgba(0, 0, 0, 0.7) 0px 30px 10px;
-  
-    .item {
-      box-shadow: rgba(0, 0, 0, 0.8) 0px 10px 5px, rgba(0, 0, 0, 0.7) 0px 10px 10px;
-      border-radius: 10px;
-      position: relative;
-      font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      grid-column: 1;
-      grid-row: 1;
-      font-size: 2.5rem;
-      width: 100%;
-      height: 100%;
-      color :black;
-      background-color: #98f5e1;
-      perspective: 100px;
-      &.empty-block{
-        display: none;
-      }
+  transform: rotateX(30deg);
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 30px 5px, rgba(0, 0, 0, 0.7) 0px 30px 10px;
+
+  .item {
+    box-shadow: rgba(0, 0, 0, 0.8) 0px 10px 5px,
+      rgba(0, 0, 0, 0.7) 0px 10px 10px;
+    border-radius: 10px;
+    position: relative;
+    font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    grid-column: 1;
+    grid-row: 1;
+    font-size: 2.5rem;
+    width: 100%;
+    height: 100%;
+    color: black;
+    background-color: #98f5e1;
+    perspective: 100px;
+    &.empty-block {
+      display: none;
     }
-  
+  }
 }
 </style>
